6ef484bb5a0170b419dd227cdb78c9d6f0c99a4a
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-01-30 20:54:28 +0100

    Add ValueError exception.

--- main.py
+++ main.py
@@ -18,49 +18,56 @@ list_of_o = []
 
 player_x = True
 winner = False
+game = True
 board = create_board()
 
 print(logo)
-print("Welcome to the Tic Tac Toe game!\n"
-      "\n"
+print("Welcome to the Tic Tac Toe game!\n\n"
       "The board looks like that:\n")
 print(create_board())
 print("\nPlayer ❌ uses ❌s and Player ⭕ uses ⭕s.\n"
       "Player ❌ begins!\n")
 
-while not winner:
-    if player_x:
-        position = int(input("Write a number where you want to put your ❌: "))
-        if position in list_of_o or position in list_of_x:
-            print("Your opponent or you already chose that place, try again.")
-        elif position not in board_positions:
-            print("Your number does not exist on board, try again.")
-        else:
-            list_of_x.append(position)
-            board = board.replace(str(board_positions[position]), "❌")
-            print(board)
-            for combo in winning_combinations:
-                if set(combo).issubset(list_of_x) and len(set(list_of_x)) >= 3:
-                    print(winner_x)
-                    print("Player ❌ is a winner!")
-                    winner = True
+while game:
+    while not winner:
+        if player_x:
+            try:
+                position = int(input("Write a number where you want to put your ❌: "))
+                if position in list_of_o or position in list_of_x:
+                    print(f"Your opponent or you have already chosen place {position}, try again.")
+                elif position not in board_positions:
+                    print("Your number does not exist on board, try again and choose number 0-8.")
                 else:
-                    player_x = False
+                    list_of_x.append(position)
+                    board = board.replace(str(board_positions[position]), "❌")
+                    print(board)
+                    for combo in winning_combinations:
+                        if set(combo).issubset(list_of_x) and len(set(list_of_x)) >= 3:
+                            print(winner_x)
+                            print("Player ❌ is a winner!")
+                            winner = True
+                        else:
+                            player_x = False
+            except ValueError:
+                print("You must choose a number between 0 and 8.")
 
-    else:
-        position = int(input("Write a number where you want to put your ⭕: "))
-        if position in list_of_o or position in list_of_x:
-            print("Your opponent or you already chose that place, try again.")
-        elif position not in board_positions:
-            print("Your number does not exist on board, try again.")
         else:
-            list_of_o.append(position)
-            board = board.replace(str(board_positions[position]), "⭕")
-            print(board)
-            for combo in winning_combinations:
-                if set(combo).issubset(list_of_o) and len(set(list_of_o)) >= 3:
-                    print(winner_o)
-                    print("Player ⭕ is a winner!")
-                    winner = True
+            try:
+                position = int(input("Write a number where you want to put your ⭕: "))
+                if position in list_of_o or position in list_of_x:
+                    print(f"Your opponent or you have already chosen place {position}, try again.")
+                elif position not in board_positions:
+                    print("Your number does not exist on board, try again and choose number 0-8.")
                 else:
-                    player_x = True
+                    list_of_o.append(position)
+                    board = board.replace(str(board_positions[position]), "⭕")
+                    print(board)
+                    for combo in winning_combinations:
+                        if set(combo).issubset(list_of_o) and len(set(list_of_o)) >= 3:
+                            print(winner_o)
+                            print("Player ⭕ is a winner!")
+                            winner = True
+                        else:
+                            player_x = True
+            except ValueError:
+                print("You must choose a number between 0 and 8.")
